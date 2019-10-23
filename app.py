d6ba9957f45bd785cd02e4d0daeda08088d72e3e
Khinshan Khan <khanshan501@gmail.com>
2019-10-23 16:27:18 -0400

    general structure for project

---
+++ app.py
@@ -0,0 +1,11 @@
+from utils import neo4j
+from utils import mongo
+
+
+def main():
+    "Executes splash screen and UI to use Hetio Net."
+    print("Welcome")
+
+
+if __name__ == "__main__":
+    main()
