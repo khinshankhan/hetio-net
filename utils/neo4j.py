20b0b6b5f4c8190bbe105f410cd19e47303e77d4
srafi1 <shakilrafi0@gmail.com>
2019-10-28 19:05:15 -0400

    create all nodes with labels using neo4j queries

--- utils/neo4j.py
+++ utils/neo4j.py
@@ -12,16 +12,22 @@ class Neo4jController():
     def __init__(self):
         self.graph = Graph()
 
+    def clear_db(self):
+        self.graph.delete_all()
+
     def create_db(self):
-        with open(os.path.join(DATA_DIR, "nodes.tsv"), "r") as nodes_file:
-            reader = csv.DictReader(nodes_file, delimiter="\t")
-            for row in reader:
-                self.graph.create(Node(row["kind"], id=row["id"], name=row["name"]))
-        with open(os.path.join(DATA_DIR, "edges.tsv"), "r") as edges_file:
-            reader = csv.DictReader(edges_file, delimiter="\t")
-            for row in reader:
-                # create relationships using merge
-                pass
+        query = """
+        USING PERIODIC COMMIT 5000
+        LOAD CSV WITH HEADERS FROM "file:/nodes.tsv" AS row FIELDTERMINATOR "\\t"
+        CREATE ({id:row.id, name:row.name, kind:row.kind});
+        """
+        self.graph.run(query)
+        labels = ["Compound", "Disease", "Gene", "Anatomy"]
+        for label in labels:
+            query = f"MATCH (n) WHERE n.kind = '{label}' SET n:{label}"
+            self.graph.run(query)
+
+        # load relationships
 
     def query_db(self, query):
         print(query)
