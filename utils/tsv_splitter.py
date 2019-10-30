e41fceaddf44629e12297fe412c155fca6b9a7aa
srafi1 <shakilrafi0@gmail.com>
2019-10-30 17:31:55 -0400

    dont rewrite tsv files if they exist already

--- utils/tsv_splitter.py
+++ utils/tsv_splitter.py
@@ -4,6 +4,8 @@ from utils.common import NEO4J_HOME, node_types, edge_types
 def write_node_files():
     for node_type in node_types:
         out_file_path = os.path.join(NEO4J_HOME, "import", f"{node_type}.tsv")
+        if os.path.exists(out_file_path):
+            continue
         print(f"Writing file for {node_type} nodes to {out_file_path}")
         command = f"echo 'id\tname\tkind' > {out_file_path}"
         os.system(command)
@@ -14,6 +16,8 @@ def write_edge_files():
     for edge_type in edge_types:
         out_file_path = os.path.join(NEO4J_HOME, "import", 
                 f"{edge_type.replace('>', '')}.tsv")
+        if os.path.exists(out_file_path):
+            continue
         print(f"Writing file for {edge_type} edges to {out_file_path}")
         command = f"echo 'source\tmetaedge\ttarget' > {out_file_path}"
         os.system(command)
